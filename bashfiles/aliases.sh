5fb8826c92aafc1474202f85f9b4a44f2649ac96
Philipe Farias and Rodrigo Ulisses <dev+philipe+rodrigo@integragrp.com.br>
2014-03-23 18:16:11 -0300

    Using git as an alias for hub.

--- bashfiles/aliases.sh
+++ bashfiles/aliases.sh
@@ -1,6 +1,11 @@
+# Basic commands
 alias ..="cd .."
 alias ...="cd ../.."
 alias ls='ls -G'
 alias ll='ls -lG'
 alias h=history
 alias x=exit
+
+
+# Dev commands
+alias git=hub # wrapper that makes git better with github
