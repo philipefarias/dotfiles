c078ba70ed3b493d2cfd05246d1d0d383b531e0f
Philipe Farias <philipefarias@gmail.com>
2016-04-18 14:09:23 -0300

    Change pg_restore alias pgr
    The `clean` parameter was raising errors when trying to drop some
    tables.
    
    Now the database must be cleaned before hand.

--- bashfiles/aliases.sh
+++ bashfiles/aliases.sh
@@ -9,5 +9,5 @@ alias x=exit
 
 # Dev commands
 alias git=hub # wrapper that makes git better with github
-alias pgr='pg_restore --clean --no-acl --no-owner -h localhost -d'
+alias pgr='pg_restore --no-acl --no-owner -h localhost -d'
 alias -- -=dash # using my custom dash script
