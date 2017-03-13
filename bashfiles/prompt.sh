4db4480a58d467f8b8e87bd2d7e4fe539dcc88a8
Philipe Farias <philipefarias@gmail.com>
2017-03-13 13:26:56 -0300

    Change bash prompt configuration
    Using a configuration for Arch Linux and Homebrew.

--- bashfiles/prompt.sh
+++ bashfiles/prompt.sh
@@ -1,4 +1,12 @@
-if [ -f /usr/local/share/gitprompt.sh ]; then
-  GIT_PROMPT_THEME=Default
-  . /usr/local/share/gitprompt.sh
+# # #
+# Using bash-git-prompt for an informative git prompt for bash
+
+# Arch Linux configuration
+if [ -f /usr/lib/bash-git-prompt/gitprompt.sh ]; then
+  source /usr/lib/bash-git-prompt/gitprompt.sh
+
+# Homebrew configuration
+elif [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
+  __GIT_PROMPT_DIR=$(brew --prefix)/opt/bash-git-prompt/share
+  source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
 fi
