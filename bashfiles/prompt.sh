2499832461d97b6e389476bc3f98c670f24c0d8e
Philipe Farias <philipefarias@gmail.com>
2018-06-25 13:38:22 -0300

    Fix git prompt on Linux
    Check if the system is macOS (Darwin) before executing a mac command.

--- bashfiles/prompt.sh
+++ bashfiles/prompt.sh
@@ -6,7 +6,7 @@ if [ -f /usr/lib/bash-git-prompt/gitprompt.sh ]; then
   source /usr/lib/bash-git-prompt/gitprompt.sh
 
 # Homebrew configuration
-elif [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
+elif [ "$(uname)" == "Darwin" ] && [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
   __GIT_PROMPT_DIR=$(brew --prefix)/opt/bash-git-prompt/share
   source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
 fi
